5bf24461e8d75ff85022102d8071e34264d9e25a
David <63205665+CatingEzz@users.noreply.github.com>
2021-03-03 13:00:20 +0200

    put `)` hm

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,4 +1,4 @@
 module.exports = async (client, guild) => {
-    client.log("JOINS", `Guild Name: "${guild.name}" (ID: ${guild.id}). (Members: ${guild.memberCount}`);
+    client.log("JOINS", `Guild Name: "${guild.name}" (ID: ${guild.id}). (Members: ${guild.memberCount})`);
     client.check(guild.id);
 };
